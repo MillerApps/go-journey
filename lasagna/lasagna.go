b6720c0344855b2a93aae84ad3db6894511647ea
MillerApps <tyemiller4@gmail.com>
2024-04-13 02:48:38 -0400

    Complete lasagna

--- lasagna/lasagna.go
+++ lasagna/lasagna.go
@@ -1,18 +1,20 @@
 package lasagna
 
-// TODO: define the 'OvenTime' constant
+const OvenTime = 40 // 40 minutes
 
 // RemainingOvenTime returns the remaining minutes based on the `actual` minutes already in the oven.
 func RemainingOvenTime(actualMinutesInOven int) int {
-	panic("RemainingOvenTime not implemented")
+	// Simple math to figure the difference
+	return OvenTime - actualMinutesInOven
 }
 
 // PreparationTime calculates the time needed to prepare the lasagna based on the amount of layers.
 func PreparationTime(numberOfLayers int) int {
-	panic("PreparationTime not implemented")
+	// Multiply by 2 to get prep time
+	return numberOfLayers * 2
 }
 
 // ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
-	panic("ElapsedTime not implemented")
+	return numberOfLayers * 2 + actualMinutesInOven
 }
