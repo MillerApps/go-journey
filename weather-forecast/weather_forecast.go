921c89d8ee7106d051ff97690468d86576bedb06
MillerApps <tyemiller4@gmail.com>
2024-04-13 22:02:52 -0400

    complete lesson

--- weather-forecast/weather_forecast.go
+++ weather-forecast/weather_forecast.go
@@ -1,8 +1,11 @@
+// Package weather represents the current conditions for the current location.
 package weather
-
+// CurrentCondition is the current weather condition.
 var CurrentCondition string
+// CurrentLocation is the current location in which the CurrentCondition takes place.
 var CurrentLocation string
 
+// Forecast provides a weather forecast for a given location.
 func Forecast(city, condition string) string {
 	CurrentLocation, CurrentCondition = city, condition
 	return CurrentLocation + " - current weather condition: " + CurrentCondition
