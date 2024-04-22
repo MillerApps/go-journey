d55f252f9a79068c1430484278fcc68a7e8cbece
MillerApps <tyemiller4@gmail.com>
2024-04-21 23:48:33 -0400

    lesson done

--- vehicle-purchase/vehicle_purchase.go
+++ vehicle-purchase/vehicle_purchase.go
@@ -2,15 +2,33 @@ package purchase
 
 // NeedsLicense determines whether a license is needed to drive a type of vehicle. Only "car" and "truck" require a license.
 func NeedsLicense(kind string) bool {
-	panic("NeedsLicense not implemented")
+	if kind == "car" || kind == "truck" {
+		return true
+	} else {
+		return false
+	}
 }
 
 // ChooseVehicle recommends a vehicle for selection. It always recommends the vehicle that comes first in lexicographical order.
 func ChooseVehicle(option1, option2 string) string {
-	panic("ChooseVehicle not implemented")
+	if option1 < option2 {
+		return option1 + " is clearly the better choice."
+	} else if option1 > option2 {
+		return option2 + " is clearly the better choice."
+	} else {
+		return ""
+	}
 }
 
 // CalculateResellPrice calculates how much a vehicle can resell for at a certain age.
 func CalculateResellPrice(originalPrice, age float64) float64 {
-	panic("CalculateResellPrice not implemented")
+	if age >= 10 {
+		return 0.50 * originalPrice
+	} else if age >= 3 {
+		return 0.70 * originalPrice
+	} else if age < 3 {
+		return 0.80 * originalPrice
+	} else {
+		return 0
+	}
 }
